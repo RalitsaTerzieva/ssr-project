764115032e8b487281b556a6e2accd2a63038892
Ralica Popova <ralicaterzieva90@gmail.com>
2024-10-10 15:57:36 +0300

    Modify App component to have gists logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
-const App = () => <div>Hello React</div>
+import { FC } from 'react';
+
+type Gist = {
+    id: string;
+    description: string;
+}
+
+type Props = {
+    gists: Gist[]
+}
+
+const App: FC<Props> = ({ gists }) => (
+    <ul>
+        {gists.map(gist => (
+            <li key={gist.id}>{gist.description}</li>
+        ))}
+    </ul>
+)
 
 export default App;
