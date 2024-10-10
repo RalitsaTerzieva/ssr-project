9df589a8250d02ccb972242e415474ee7f492b25
Ralica Popova <ralicaterzieva90@gmail.com>
2024-10-10 16:44:11 +0300

    Change client and server

--- src/client.tsx
+++ src/client.tsx
@@ -1,11 +1,21 @@
-import { createRoot } from 'react-dom/client';
+import { hydrateRoot } from 'react-dom/client';
 import App from './App';
+
+type Gist = {
+  id: string;
+  description: string;
+};
+
+declare global {
+  interface Window {
+    gists: Gist[];
+  }
+}
 
 const container = document.getElementById('app');
 
 if (container) {
-    const root = createRoot(container);
-    root.render(<App />);
+  hydrateRoot(container, <App gists={window.gists} />);
 } else {
-    console.error("Element with ID 'app' not found");
+  console.error("Element with ID 'app' not found");
 }
--- src/server.tsx
+++ src/server.tsx
@@ -3,16 +3,21 @@ import { renderToString } from 'react-dom/server'
 import path from 'path'
 import App from './App'
 import template from './template'
+import fetch from 'isomorphic-fetch';
 
 const app = express()
 
 app.use(express.static(path.resolve(__dirname, './dist/public')))
 
+// @ts-expect-error
 app.get('/', (req: Request, res: Response) => {
-  console.log('Rendering App', req)
-  const body = renderToString(<App />)
-  const html = template(body)
-  res.send(html)
+  fetch('https://api.github.com/users/gaearon/gists')
+    .then(response => response.json())
+    .then(gists => {
+      const body = renderToString(<App gists={gists}/>)
+      const html = template(body);
+      res.send(html);
+    })
 })
 
 app.listen(3000, () => console.log('Listening on port 3000'))
